b068efb8b1a423770ac1f9bd7455deb86b14d7a1
llfalcao <luizotavio-falcao@hotmail.com>
2021-07-05 01:10:51 -0300

    Update the single round function's return values

--- rps.js
+++ rps.js
@@ -1,21 +1,20 @@
 function computerPlay() {
-    let plays = ['Rock', 'Paper', 'Scissors'];
+    const plays = ['Rock', 'Paper', 'Scissors'];
     return plays[Math.floor(Math.random() * plays.length)];
 }
 
 function playRound(player, computer) {
-    let ps = player.charAt(0).toUpperCase() + player.substring(1, player.length).toLowerCase();
-    let cs = computer;
-    let msg = `[Player] ${ps} vs ${cs} [Computer]`
+    const ps = player.charAt(0).toUpperCase() + player.substring(1, player.length).toLowerCase();
+    const cs = computer;
     if (ps === cs) {
-        return `Tie\n ${msg}`
+        return `Tie! Both played ${ps}.`
     } else {
         if (ps === 'Rock' && cs === 'Scissors' ||
             ps === 'Scissors' && cs === 'Paper' ||
             ps === 'Paper' && cs === 'Rock') {
-            return `Player wins!\n${msg}`
+            return `You win! ${ps} beats ${cs}.`
         } else {
-            return `Computer wins!\n${msg}`
+            return `Computer wins! ${cs} beats ${ps}.`
         }
     }
 }
