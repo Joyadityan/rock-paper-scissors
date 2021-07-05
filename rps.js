13a0376cd299a3a05cf0d292a85cf2235cd4e60a
llfalcao <luizotavio-falcao@hotmail.com>
2021-07-05 02:01:54 -0300

    Add game function and update return values for each round

--- rps.js
+++ rps.js
@@ -3,21 +3,56 @@ function computerPlay() {
     return plays[Math.floor(Math.random() * plays.length)];
 }
 
-function playRound(player, computer) {
-    const ps = player.charAt(0).toUpperCase() + player.substring(1, player.length).toLowerCase();
-    const cs = computer;
+function playRound(ps, cs) {
     if (ps === cs) {
-        return `Tie! Both played ${ps}.`
+        return '0';
     } else {
         if (ps === 'Rock' && cs === 'Scissors' ||
             ps === 'Scissors' && cs === 'Paper' ||
             ps === 'Paper' && cs === 'Rock') {
-            return `You win! ${ps} beats ${cs}.`
+            return '1';
         } else {
-            return `Computer wins! ${cs} beats ${ps}.`
+            return '2';
         }
     }
 }
 
-const playerSelection = 'papEr';
-const computerSelection = computerPlay();
+function game() {
+    let playerSelection;
+    let computerSelection;
+
+    let playerScore = 0;
+    let computerScore = 0;
+    let result;
+    for (let round = 1; round <= 5; round++) {
+        playerSelection = window.prompt("Rock, Paper, or Scissors?");
+        computerSelection = computerPlay();
+
+        playerSelection = playerSelection.charAt(0).toUpperCase() +
+            playerSelection.substring(1, playerSelection.length).toLowerCase();
+
+        console.log(`[Round ${round}]`);
+
+        result = playRound(playerSelection, computerSelection);
+        if (result == '0') {
+            console.log(`Tie! Both played ${playerSelection}.`);
+            console.log(`Score: ${playerScore} - ${computerScore}`);
+        } else if (result == '1') {
+            console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
+            console.log(`Score: ${++playerScore} - ${computerScore}`);
+        } else {
+            console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
+            console.log(`Score: ${playerScore} - ${++computerScore}`);
+        }
+        console.log('\n')
+    }
+    if (playerScore > computerScore) {
+        return "You win!";
+    } else if (computerScore > playerScore) {
+        return "You lose!";
+    } else {
+        return "It's a draw!";
+    }
+}
+
+game();
