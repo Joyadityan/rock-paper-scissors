fb3720fd0e02bcaa90ec60bd3729e100debf8c11
llfalcao <luizotavio-falcao@hotmail.com>
2021-07-05 00:46:02 -0300

    Add single round function

--- rps.js
+++ rps.js
@@ -2,3 +2,23 @@ function computerPlay() {
     let plays = ['Rock', 'Paper', 'Scissors'];
     return plays[Math.floor(Math.random() * plays.length)];
 }
+
+function playRound(player, computer) {
+    let ps = player.charAt(0).toUpperCase() + player.substring(1, player.length).toLowerCase();
+    let cs = computer;
+    let msg = `[Player] ${ps} vs ${cs} [Computer]`
+    if (ps === cs) {
+        return `Tie\n ${msg}`
+    } else {
+        if (ps === 'Rock' && cs === 'Scissors' ||
+            ps === 'Scissors' && cs === 'Paper' ||
+            ps === 'Paper' && cs === 'Rock') {
+            return `Player wins!\n${msg}`
+        } else {
+            return `Computer wins!\n${msg}`
+        }
+    }
+}
+
+const playerSelection = 'papEr';
+const computerSelection = computerPlay();
